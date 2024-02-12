5bf9f0af832b6b204455826a3c0454c612f30f04
Christopher DeVito <christopher.devito@protonmail.com>
2024-02-12 04:26:35 -0500

    moved loadAdjacency to Helpers

--- Sources/zxcvbn-swift/Matcher.swift
+++ Sources/zxcvbn-swift/Matcher.swift
@@ -452,7 +452,7 @@ struct MatchResources {
 
     private init() {
         dictionaryMatchers = loadFrequencyLists()
-        graphs = loadAdjacencyGraphs()
+        graphs = Helpers.shared.loadAdjacencyGraphs()
     }
 
     private func loadFrequencyLists() -> [MatcherBlock] {
@@ -477,21 +477,6 @@ struct MatchResources {
         } catch {
             print("Error parsing frequency lists: \(error)")
             return []
-        }
-    }
-
-    private func loadAdjacencyGraphs() -> [String: [String: [String?]]] {
-        guard let path = Bundle.module.url(forResource: "adjacency-graphs", withExtension: "json"),
-              let data = try? Data(contentsOf: path) else {
-            return [:]
-        }
-
-        do {
-            let json = try JSONSerialization.jsonObject(with: data) as! [String: [String: [String]]]
-            return json
-        } catch {
-            print("Error parsing adjacency graphs: \(error)")
-            return [:]
         }
     }
 
---
+++ Sources/zxcvbn-swift/helpers.swift
@@ -0,0 +1,21 @@
+
+
+import Foundation
+
+struct Helpers {
+    static let shared = Helpers()
+    func loadAdjacencyGraphs() -> [String: [String: [String?]]] {
+        guard let path = Bundle.module.url(forResource: "adjacency-graphs", withExtension: "json"),
+              let data = try? Data(contentsOf: path) else {
+            return [:]
+        }
+
+        do {
+            let json = try JSONSerialization.jsonObject(with: data) as! [String: [String: [String]]]
+            return json
+        } catch {
+            print("Error parsing adjacency graphs: \(error)")
+            return [:]
+        }
+    }
+}
