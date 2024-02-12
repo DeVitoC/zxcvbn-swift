6936ece682af4f174f3a66c22761061c90929391
Christopher DeVito <christopher.devito@protonmail.com>
2024-02-11 22:24:01 -0500

    Add resources adjacency-graphs and frequency-lists

--- Package.swift
+++ Package.swift
@@ -15,7 +15,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "zxcvbn-swift"),
+            name: "zxcvbn-swift",
+            resources: [
+                .copy("Resources/adjacency-graphs.json"),
+                .copy("Resources/frequency-lists.json")
+            ]
+        ),
         .testTarget(
             name: "zxcvbn-swiftTests",
             dependencies: ["zxcvbn-swift"]),
