9231f736ade6af8a75d03a551da6e827f97c96dc
Christopher DeVito <christopher.devito@protonmail.com>
2024-02-18 19:59:18 -0500

    Fix testNChooseK and add setUp method

--- Tests/zxcvbn-swiftTests/test_scoring.swift
+++ Tests/zxcvbn-swiftTests/test_scoring.swift
@@ -10,27 +10,33 @@ func approxEqual(_ actual: Double, _ expected: Double) -> Bool {
 }
 
 final class testScoring: XCTestCase {
+    var scoring: Scoring!
 
+    override func setUp() {
+        super.setUp()
+        scoring = Scoring()
+    }
     func testNChoosek() {
-      let nChooseK = Scoring.nChoosek
-      let testCases = [
-          (n: 0, k: 0, result: 1),
-          (n: 1, k: 0, result: 1),
-          (n: 5, k: 0, result: 1),
-          (n: 0, k: 1, result: 0),
-          (n: 0, k: 5, result: 0),
-          (n: 2, k: 1, result: 2),
-          (n: 4, k: 2, result: 6),
-          (n: 33, k: 7, result: 4272048)
-      ]
+        let nChooseK = scoring.nChooseK
+        let testCases = [
+            (n: 0, k: 0, result: 1),
+            (n: 1, k: 0, result: 1),
+            (n: 5, k: 0, result: 1),
+            (n: 0, k: 1, result: 0),
+            (n: 0, k: 5, result: 0),
+            (n: 2, k: 1, result: 2),
+            (n: 4, k: 2, result: 6),
+            (n: 33, k: 7, result: 4272048)
+        ]
 
-      for testCase in testCases {
-          XCTAssertEqual(nChoosek(testCase.n, testCase.k), testCase.result, "nChoosek(\(testCase.n), \(testCase.k)) == \(testCase.result)")
-      }
+        for testCase in testCases {
+            XCTAssertEqual(nChooseK(testCase.n, testCase.k), testCase.result, "nChoosek(\(testCase.n), \(testCase.k)) == \(testCase.result)")
+        }
 
-      let n = 49
-      let k = 12
-      XCTAssertEqual(nChoosek(n, k), nChoosek(n, n-k), "mirror identity")
-      XCTAssertEqual(nChoosek(n, k), nChoosek(n-1, k-1) + nChoosek(n-1, k), "pascal's triangle identity")
-  }
+        let n = 49
+        let k = 12
+        XCTAssertEqual(nChooseK(n, k), nChooseK(n, n-k), "mirror identity")
+        XCTAssertEqual(nChooseK(n, k), nChooseK(n-1, k-1) + nChooseK(n-1, k), "pascal's triangle identity")
+    }
+
 }
